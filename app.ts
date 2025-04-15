8b3c2af9dd0e55bedd29dc01410308e53144ca58
Jimba Abdullahi Olakunle <jimbaabdullahi9@gmail.com>
2025-04-15 04:11:03 +0100

    Added comments

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
+// Define an interface that describes the structure of a Vehicle
 interface Vehicle{
-    make:string,
-    model:string,
-    year:number,
-    start(): void
+    make:string, // Manufacturer of the vehicle
+    model:string, // Model of the vehicle
+    year:number,    // Year the vehicle was manufactured
+    start(): void // Method to start the vehicle; returns nothing (void)
 }
 
+// Define a Car class that implements the Vehicle interface
 class Car implements Vehicle{
     constructor(public make:string,public model:string,public year:number){
         this.make = make;
@@ -16,6 +18,8 @@ class Car implements Vehicle{
     }
 }
 
+// creating an instance of the car.
 const car1 = new Car("Lexus", "ES350", 2019);
 
+// call the function start().
 car1.start();
